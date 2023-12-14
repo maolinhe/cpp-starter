b425a867ca17396ca7d387ba4ad1f6d64ec92169
maolin.he <1423530345@qq.com>
2023-12-14 15:33:12 +0800

    Update git ignore

--- src/include/namespace_test.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-namespace SpaceA {
-void print() { std::cout << "print namespace A" << std::endl; }
-
-namespace SpaceAA {
-void print() { std::cout << "print namespace AA" << std::endl; }
-} // namespace SpaceAA
-} // namespace SpaceA
--- src/include/std_input_output.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-void std_input_output()
-{
-  int val;
-  std::cin >> val;
-  std::cout << "cout:" << val << std::endl;
-  std::cerr << "cerr:" << val << std::endl;
-}
